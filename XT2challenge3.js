705c87875182cb8dd61dc4d100b5f50584023f6e
Milou de Koning <miloudekoning@MacBook-Pro-van-Milou.local>
2020-04-06 17:14:50 +0200

    Na les

--- XT2challenge3.js
+++ XT2challenge3.js
@@ -1,62 +1,30 @@
-function getAPIdata() {
+// Ophalen data uit externe API
+fetch('https://api.covid19api.com/summary')
 
-	var url = '';
-	var apiKey = '';
-	var city = '';
+  .then(function(response) {
+    // formatteer de variabele response naar json
+    return response.json();
+  })
 
-	//opbouwen request
-	var request = url + '?' + 'appid' + apiKey + '&' + 'q=' + city;
+  .then(function(result) {
+    // hier heb ik de data uit api beschikbaar
+    // console.log(result.Countries);
+    var countries = result.Countries;
 
-	//huidige weer ontvangen
-	fetch(request)
+    // nu heb ik de landen uit de covid API in een variabele met de "countries" beschikbaar
 
-	//ontleden naar JSON formaat
-	.then(function(response) {
-		//controle of dat wat je terugkrijgt ok is
-		if(!response.ok) thow Error(response.statusText);
-		//als er een error is stopt de code hier
-		return response.json();
-	})
-
-	//toon weer per dag
-	.then(function(response) {
-		//toon weatherCondition
-		//functie 1:
-		onAPISucces(response);
-	})
-
-	//vang error
-	//functie 2: als dat ophalen niet lukt dan ziet de bezoeker dit
-	.catch(function(error) {
-		onAPIError(error);
-	});
-}
-
-//krijgt weer die response mee
-//wanneer je succes hebt met de API dan pas kan er wat mee worden gedaan
-//het 'mooi' maken van de data voor op de website
-function onAPISucces(response) {
-	//krijg weertype in string formaat
-	var description = response.weather[0].description;
-
-	//krijg temperatuur in Celcius
-	var degC = Math.floor(response.main.temp - 273.15);
-
-	//toon weer in DOM
-	var weatherBox = document.getElementById('weather');
-	weatherBox.innerHTML = degC + '&#176;C' + type;
-}
-
-function onAPIError(error) {
-	console.error('Request failed', error);
-	var weatherBox = document.getElementById('weather');
-	weatherBox.className = 'hidden';
-	weatherBox.innerHTML = 'Something went wrong in the request. Could not fetch weather.'
-}
-
-//'init' gegevensstroom
-getAPIdata();
-
+    // we gaan door countries heen loopen
+    countries.forEach(function(country) {
+      if(country.Country == "Germany") {
+        // run dit stukje code hieronder alleen als de countrynaam Germany is
+        document.getElementById('land').innerHTML = country.Country;
+        document.getElementById('confirmed').innerHTML = country.TotalConfirmed;
+      }
+    });
+  });
+ 
+//for (var i=0; i<countries.length; i++){
+//alert(country.Country);
 
 
 
