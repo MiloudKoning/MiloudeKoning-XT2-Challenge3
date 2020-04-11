ebc7ca7814637d95c6ce0d2cb3d22b49b4124e18
Milou de Koning <miloudekoning@MacBook-Pro-van-Milou.local>
2020-04-11 12:24:56 +0200

    nee het weer werkt nog steeds niet via github :unamused:

--- script/newXT2challenge3.js
+++ script/newXT2challenge3.js
@@ -44,51 +44,6 @@ var map = new mapboxgl.Map({
 	zoom: 2,
 });
 
-var countries = [
-  {
-    // SpaceX Seattle
-    name: 'Redmond',
-    coordinates: [-122.035494, 47.693214]
-  },
-
-  {
-    // SpaceX Landing Zone 4
-    name: 'Lompoc',
-    coordinates: [-120.614561, 34.633670]
-  },
-
-  {
-    // SpaceX
-    name: 'Hawthorne',
-    coordinates: [-118.328263, 33.921285]
-  },
-
-  {
-    // SpaceX Space Launch Facility
-    name: 'Brownsville',
-    coordinates: [-97.180926, 25.993464]
-  },
-
-  {
-    // SpaceX Rocket Development Facility
-    name: 'McGregor',
-    coordinates: [-97.437303, 31.434422]
-  },
-
-  {
-    // SpaceX Landing Zone 1 & 2
-    name: 'Cape Canaveral',
-    coordinates: [-80.601714, 28.610747]
-  },
-
-  {
-    // SpaceX Washington DC
-    name: 'Washington',
-    coordinates: [-77.024999, 38.901388]
-  },
-
-];
-
 // url aangesproken wordt op openWeather
 var openWeatherMapUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
